a3c0351bb1685c25750c53420f28f387c08faeda
sagarkapadia12 <131685515+sagarkapadia12@users.noreply.github.com>
2023-06-05 15:06:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,147 @@
+(function($) {
+  "use strict"
 
-    var filmname = document.getElementById("ongoing3name");
-    var filmdes = document.getElementById("ongoing3des");
-    var image = document.getElementById("image");
+  // NAVIGATION
+  var responsiveNav = $('#responsive-nav'),
+    catToggle = $('#responsive-nav .category-nav .category-header'),
+    catList = $('#responsive-nav .category-nav .category-list'),
+    menuToggle = $('#responsive-nav .menu-nav .menu-header'),
+    menuList = $('#responsive-nav .menu-nav .menu-list');
 
+  catToggle.on('click', function() {
+    menuList.removeClass('open');
+    catList.toggleClass('open');
+  });
 
-    // var name = filmname.value;
-    // var des = filmdes.value;
+  menuToggle.on('click', function() {
+    catList.removeClass('open');
+    menuList.toggleClass('open');
+  });
 
+  $(document).click(function(event) {
+    if (!$(event.target).closest(responsiveNav).length) {
+      if (responsiveNav.hasClass('open')) {
+        responsiveNav.removeClass('open');
+        $('#navigation').removeClass('shadow');
+      } else {
+        if ($(event.target).closest('.nav-toggle > button').length) {
+          if (!menuList.hasClass('open') && !catList.hasClass('open')) {
+            menuList.addClass('open');
+          }
+          $('#navigation').addClass('shadow');
+          responsiveNav.addClass('open');
+        }
+      }
+    }
+  });
 
-    const firebaseref = firebase.database().ref();
+  // HOME SLICK
+  $('#home-slick').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+  });
 
-    firebaseref.child('Films').limitToLast(1).on("value", function(snapshot) {
-        console.log(snapshot.val());
+  // PRODUCTS SLICK
+  $('#product-slick-1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-1',
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 
-        snapshot.forEach(function(data) {
-            var alldata = data.val()
-            console.log(data.key);
+  $('#product-slick-2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-2',
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 
-            
-            filmname.innerText = alldata.Name;
-            filmdes.innerText = alldata.Description;
-            image.src = "#";
-            
+  // PRODUCT DETAILS SLICK
+  $('#product-main-view').slick({
+    infinite: true,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    fade: true,
+    asNavFor: '#product-view',
+  });
 
-        });
+  $('#product-view').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+    asNavFor: '#product-main-view',
+  });
+
+  // PRODUCT ZOOM
+  $('#product-main-view .product-view').zoom();
+
+  // PRICE SLIDER
+  var slider = document.getElementById('price-slider');
+  if (slider) {
+    noUiSlider.create(slider, {
+      start: [1, 999],
+      connect: true,
+      tooltips: [true, true],
+      format: {
+        to: function(value) {
+          return value.toFixed(2) + '$';
+        },
+        from: function(value) {
+          return value
+        }
+      },
+      range: {
+        'min': 1,
+        'max': 999
+      }
     });
+  }
 
-    
+})(jQuery);
